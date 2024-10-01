df52481a09e4ba7913b80f1f507f2bec0148ecc3
glen <glen84_dr@hotmail.com>
2024-10-02 00:04:12 +0200

    refactoring code

--- py_project/main.py
+++ py_project/main.py
@@ -1,30 +1,44 @@
+import sys, csv
 from one_book import scrap_one_book
 from category import send_list_books
 from all_books import send_all_books_scraped
 
-# Function that will create the csv based on the case           
+
+# Function that will be used to create a csv doc in order of the
+
 def create_csv(name_csv,list_books):
-    with open(f'{name_csv}.csv', 'w') as category_file:
-        category_file.write("Url"+','+'UPC'+','+'Title'+','+'Price including tax'+','
-            +'Price excluding tax'+','+'Category'+','+'Review rating'+','+'Image Url \n')
-        # Using loop to select all the books
+    # creating field names from keys
+    field_names = [x for x in list_books[0].keys()]
+
+    with open(f'{name_csv}.csv', 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=field_names)
+        writer.writeheader()
         for book_obj in list_books:
-            category_file.write(
-            f"{book_obj['url']}" + ','+
-            book_obj['upc']+','+
-            book_obj['title']+','+
-            book_obj['price_including_tax']+','+
-            book_obj['price_excluding_tax']+','+
-            book_obj['category']+','+
-            book_obj['review_rating']+','+
-            book_obj['image_url']+'\n'
-        )
+            writer.writerow(book_obj)
 
-# Creating csv for one book
-# create_csv('book', [scrap_one_book()])
 
-# Creating csv file for one category
-create_csv('books_category', send_list_books())
+print("Here are three cases of parsing the books and saving their data to a csv file:")
+print("  A --- Parsing one book")
+print("  B --- Parsing all the books of one category ")
+print("  C --- Parsing all the books of every category")
+print(" Please choose one of the cases A, B or C")
+abc = input()
+i = 1
+while not abc in ['A','B','C']:
+    print(f"You choosed {abc}. Make sure your choice is A, B or C")
+    i +=1
+    abc = input()
+    if i > 2:
+        sys.exit("Too many errors please restart over again.")
 
-# Creating csv file for all books
-# create_csv('all_books', send_all_books_scraped())
+
+match abc:
+    case 'A':
+        # Creating csv for one book
+        create_csv('book', [scrap_one_book()])
+    case 'B':
+        # Creating csv file for one category
+        create_csv('books_category', send_list_books())
+    case 'C':
+        # Creating csv file for all books
+        create_csv('all_books', send_all_books_scraped())
--- py_project/one_book.py
+++ py_project/one_book.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 def scrap_one_book(given_url=None):
     
-    if given_url != None:
+    if given_url is not None:
         url = given_url
     else:
         print("Entrez le lien de la page: ")
@@ -12,7 +12,7 @@ def scrap_one_book(given_url=None):
 
     try:
         response = requests.get(url)
-    except:
+    except Exception:
         sys.exit("Be carefull the domain name is wrong! Please restart from the begining.")
 
 
@@ -22,28 +22,26 @@ def scrap_one_book(given_url=None):
         url = input()
         try:
             response = requests.get(url)
-        except:
+        except Exception:
             sys.exit("Be carefull the domain name is wrong! Please restart from the begining.")
 
-    page_dt_dict = {'url': url}
+    page_dt_dict = {'url': url} # renomer le variable
 
     bs_body = BeautifulSoup(response.text, 'html.parser').find('body')
-   
-    page_dt_dict['description'] = bs_body.find_all('p')[3].text
     page_dt_dict['title'] = bs_body.find('h1').text
-    img_url = bs_body.find('img', alt=page_dt_dict['title']).get('src')
-    page_dt_dict['image_url'] = img_url
     # Identify the table with prices, upc, rating, number avaible
     table_prices = bs_body.find('table').find_all('tr')
     page_dt_dict['upc'] = table_prices[0].td.text
     page_dt_dict['price_including_tax'] = table_prices[3].td.text
     page_dt_dict['price_excluding_tax'] = table_prices[2].td.text
-    page_dt_dict['number_available'] = table_prices[5].td.text
-    page_dt_dict['review_rating'] = table_prices[6].td.text
-
     # Category
-    navbar = bs_body.find_all("ul")
-    category = (navbar)[0].find_all('li')[2].text
+    navbar = bs_body.find("ul")
+    category = navbar.find_all('li')[2].text
     page_dt_dict['category'] = category.replace('\n','')
+    page_dt_dict['review_rating'] = table_prices[6].td.text
+    img_url = bs_body.find('img', alt=page_dt_dict['title']).get('src')
+    page_dt_dict['image_url'] = img_url
+    page_dt_dict['number_available'] = table_prices[5].td.text
+    page_dt_dict['description'] = bs_body.find_all('p')[3].text
 
     return page_dt_dict
